9879eb0c365e854a871e15fb47519c9cc9eaee39
Ian Chiu <ianchiu@shacom.com.tw>
2017-08-24 18:10:53 +0800

    no message

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function number() {
           var resp = request.responseText;
           // console.log('response: '+resp);
           var array = resp.qaDataList;
-          foreach(array, function(entryIndex, entry) {
+          array.forEach(function(entryIndex, entry) {
             console.log(entry.topic, entry.content);
           });
 
