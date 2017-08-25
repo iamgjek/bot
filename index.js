3c8f9fb6717be30d78f4c90b34ed6906e7042569
Ian Chiu <ianchiu@shacom.com.tw>
2017-08-25 17:35:13 +0800

    no message

--- index.js
+++ index.js
@@ -71,20 +71,18 @@ function number() {
           var array = resp.qaDataList;
 
           if (msg.indexOf('hello') != -1 || msg.indexOf('hi') != -1) {
-              replyMsg = 'Hi! 請輸入要查詢的問題～';
-          } else if (msg.indexOf(msg) != -1) {
-
-            if (array) {
-              // console.log('array: '+array.length);
-              for (var i = 0; i < array.length; i++) {
-                newArray[i] = '  '+array[i].topic+' '+rUrl+array[i].topic+'\n\n';
-              }
-              replyMsg = '你要找的是 "'+msg+'"...\n 機器人建議的結果為：\n\n'+newArray;
-            } else {
-              newArray = '請問你輸入的這是什麼鬼... 看不懂啦 (╯‵□′)╯︵┴─┴';
-              replyMsg = newArray;
+            replyMsg = 'Hi! 請輸入要查詢的問題～';
+          } else if (msg.indexOf('TFE') != -1) {
+            replyMsg = 'TFE台灣資金交易所－標會型P2P借貸！\n大幅改良流傳千年的民間「標會」機制，使用者在平台可以自主決定借款利率，解決了當今網路金融直接存借的問題。除此之外，使用者投資，建立信用等人生各階段金融需求皆可在平台上滿足，開創傳統銀行之外的另一個新選擇。\nhttp://tfe.tw';
+          } else if (array) {
+            // console.log('array: '+array.length);
+            for (var i = 0; i < array.length; i++) {
+              newArray[i] = ' '+array[i].topic+' '+rUrl+array[i].topic+'\n\n';
             }
-
+            replyMsg = '你要找的是 "'+msg+'"...\n機器人建議的結果為：\n\n'+newArray;
+          } else {
+            newArray = '請問你輸入的這是什麼鬼... 看不懂啦 (╯‵□′)╯︵┴─┴';
+            replyMsg = newArray;
           }
 
           event.reply(replyMsg).then(function(data) {
