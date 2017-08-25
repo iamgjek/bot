fc059bc1940bea5d0d5f07c53387fdd8d6a3d631
Ian Chiu <ianchiu@shacom.com.tw>
2017-08-25 17:46:24 +0800

    no message

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function number() {
       request.open('POST', 'https://www.taiwanfundexchange.com.tw/TFEFrontend/qaQuery', true);
       request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
       // request.responseType = "resp";
-      request.send('queryText='+msg'\w');
+      request.send('queryText='+msg);
       request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
           // Success!
