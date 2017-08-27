62a066581fc5852308a69142b6628834468860a8
Ian <iamgjek@gmail.com>
2017-08-28 00:49:20 +0800

    add template button & carousel

--- index.js
+++ index.js
@@ -41,34 +41,88 @@ function number() {
       var request = new XMLHttpRequest();
       var rUrl = 'https://www.taiwanfundexchange.com.tw/TFEFrontend/qa?queryText=';
       var newArray = [];
-      var template = 
-        {
-          "type": "template",
-          "altText": "this is a buttons template",
-          "template": {
-              "type": "buttons",
-              "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-              "title": "Menu",
-              "text": "Please select",
-              "actions": [
-                  {
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=123"
-                  },
-                  {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=123"
-                  },
-                  {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/123"
-                  }
-              ]
-          }
-        };
+      var tmp_carousel = 
+      {
+        "type": "template",
+        "altText": "this is a carousel template",
+        "template": {
+            "type": "carousel",
+            "columns": [
+                {
+                  "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                  "title": "this is menu",
+                  "text": "description",
+                  "actions": [
+                      {
+                          "type": "postback",
+                          "label": "Buy",
+                          "data": "action=buy&itemid=111"
+                      },
+                      {
+                          "type": "postback",
+                          "label": "Add to cart",
+                          "data": "action=add&itemid=111"
+                      },
+                      {
+                          "type": "uri",
+                          "label": "View detail",
+                          "uri": "http://example.com/page/111"
+                      }
+                  ]
+                },
+                {
+                  "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                  "title": "this is menu",
+                  "text": "description",
+                  "actions": [
+                      {
+                          "type": "postback",
+                          "label": "Buy",
+                          "data": "action=buy&itemid=222"
+                      },
+                      {
+                          "type": "postback",
+                          "label": "Add to cart",
+                          "data": "action=add&itemid=222"
+                      },
+                      {
+                          "type": "uri",
+                          "label": "View detail",
+                          "uri": "http://example.com/page/222"
+                      }
+                  ]
+                }
+            ]
+        }
+      };
+      var tmp_button = 
+      {
+        "type": "template",
+        "altText": "this is a buttons template",
+        "template": {
+            "type": "buttons",
+            "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+            "title": "Menu",
+            "text": "Please select",
+            "actions": [
+                {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=123"
+                },
+                {
+                  "type": "postback",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=123"
+                },
+                {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/123"
+                }
+            ]
+        }
+      };
       request.open('POST', 'https://www.taiwanfundexchange.com.tw/TFEFrontend/qaQuery', true);
       request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
       request.send('queryText='+msg);
@@ -80,11 +134,14 @@ function number() {
           // console.log('response: \n'+resp);
           var array = resp.qaDataList;
 
-          if (msg.indexOf('hello') != -1 || msg.indexOf('hi') != -1 || msg.indexOf('Hello') != -1 || msg.indexOf('Hi') != -1 || msg.indexOf('哪位') != -1 || msg.indexOf('說話') != -1 || msg.indexOf('嗎') != -1) {
+          if (msg.indexOf('1') != -1 ) {
+            replyMsg = tmp_carousel;
+          } else if (msg.indexOf('2') != -1 ) {
+            replyMsg = tmp_button;
+          } else if (msg.indexOf('hello') != -1 || msg.indexOf('hi') != -1 || msg.indexOf('Hello') != -1 || msg.indexOf('Hi') != -1 || msg.indexOf('哪位') != -1 || msg.indexOf('說話') != -1 || msg.indexOf('嗎') != -1) {
             replyMsg = '請不要跟機器人聊天，以免影響工作效率... 要把妹去去去～';
           } else if (msg.indexOf('TFE') != -1 || msg.indexOf('tfe') != -1 || msg.indexOf('Tfe') != -1) {
-            // replyMsg = 'TFE台灣資金交易所－標會型P2P借貸！\n大幅改良流傳千年的民間「標會」機制，使用者在平台可以自主決定借款利率，解決了當今網路金融直接存借的問題。除此之外，使用者投資，建立信用等人生各階段金融需求皆可在平台上滿足，開創傳統銀行之外的另一個新選擇。\n立即前往 >> http://tfe.tw';
-            replyMsg = template;
+            replyMsg = 'TFE台灣資金交易所－標會型P2P借貸！\n大幅改良流傳千年的民間「標會」機制，使用者在平台可以自主決定借款利率，解決了當今網路金融直接存借的問題。除此之外，使用者投資，建立信用等人生各階段金融需求皆可在平台上滿足，開創傳統銀行之外的另一個新選擇。\n立即前往 >> http://tfe.tw';
           } else if (msg.indexOf('我要') != -1 ) {
             replyMsg = '要你個大頭啦～\n\n請輸入關鍵字，建議用單詞來進行搜尋，例如：開標、信用額度等等';
           } else if (msg.indexOf('我想') != -1 ) {
